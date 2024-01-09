0b0a47e272caa582cb4ec1db28e58a93f9168097
marcelldac <dacmarcell@gmail.com>
2024-01-09 12:57:36 -0300

    feat: translate commands, add repetir and gatos commands, add dotenv

--- app.ts
+++ app.ts
@@ -1,13 +1,20 @@
+import dotenv from 'dotenv';
 import axios from 'axios';
 import TelegramBot from 'node-telegram-bot-api';
-const token = '6673606499:AAHviZM4lCGYb7x25Cw0CXjLkGoqCZ2P5GE';
-const bot = new TelegramBot(token, { polling: true });
+
+dotenv.config();
+const telegramToken = process.env.TELEGRAM_TOKEN;
+const jokeApiLanguage = '&lang=pt';
+const jokeApiBlacklistFlags = `blacklistFlags=nsfw,racist,sexist,explicit${jokeApiLanguage}`;
+const jokeApiURL = `https://v2.jokeapi.dev/joke/Any?${jokeApiBlacklistFlags}`;
+
+if (!telegramToken) throw new Error('telegramToken in env not found.');
+
+const bot = new TelegramBot(telegramToken, { polling: true });
 
 async function fetchJokes() {
   try {
-    const { data } = await axios.get(
-      'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,sexist,explicit&lang=pt' // restric results with flags
-    );
+    const { data } = await axios.get(jokeApiURL); // restric results with flags
     const joke = {
       setup: data.setup,
       punchline: data.delivery,
@@ -20,12 +27,32 @@ async function fetchJokes() {
 }
 
 bot.onText(/\/start/, (msg, match) => {
-  bot.sendMessage(msg.chat.id, 'Hello!');
+  const chatID = msg.chat.id;
+  bot.sendMessage(
+    chatID,
+    '[TeTa] Olá! Seja bem-vindo ao melhor bot do telegram: o TeTa bot!\n\nAqui temos piadas com /piada\nrepetição com /repetir   [msg] '
+  );
 });
 
-bot.onText(/\/joke/, async (msg) => {
+bot.onText(/\/piada/, async (msg) => {
+  const chatID = msg.chat.id;
   const joke = await fetchJokes();
   const response = `${joke.setup} ${joke.punchline}`;
-  bot.sendMessage(msg.chat.id, response);
+  bot.sendMessage(chatID, response);
   //fetch joke
+});
+
+bot.onText(/\/repetir (.+)/, (msg, match) => {
+  const chatID = msg.chat.id;
+  if (match) {
+    const res = match[1];
+    bot.sendMessage(chatID, `[TeTa] ${res}`);
+  } else {
+    bot.sendMessage(chatID, '[TeTa] Conteúdo não encontrado.');
+  }
+});
+
+bot.onText(/\/gatos/, async (msg) => {
+  const chatID = msg.chat.id;
+  /* const { data } = axios.get(); */
 });
